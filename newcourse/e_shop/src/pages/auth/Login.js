f15196bec2822bdb9057ef26c7c1f65b71ec372e
HsiehJerry <jerry.hsieh@garmin.com>
2022-09-03 15:15:43 +0800

    update log & register

--- newcourse/e_shop/src/pages/auth/Login.js
+++ newcourse/e_shop/src/pages/auth/Login.js
@@ -1,47 +1,84 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./auth_module.scss";
 import img_Login from "../../assets/login.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { IoLogoGoogle } from "react-icons/io";
 import Card from "../../Component/card/Card";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/config";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Loader from "../../Component/loader/Loader";
 const Login = () => {
+  const Email = useRef(null);
+  const Password = useRef(null);
+  const [isloading, setisLoading] = useState(false);
+  const navigate = useNavigate();
+  const fun_Login = (e) => {
+    e.preventDefault();
+    const currentEmail = Email.current.value;
+    const currentPassword = Password.current.value;
+    setisLoading(true);
+    signInWithEmailAndPassword(auth, currentEmail, currentPassword)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setisLoading(false);
+        toast.success("login Successful!!");
+        navigate("/");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+        setisLoading(false);
+      });
+  };
+
   return (
-    <section>
-      <div className="auth container">
-        <div className="img">
-          <img src={img_Login} alt="login" />
-        </div>
-        <Card>
-          <div className="form">
-            <h2>Login</h2>
-            <form>
-              <input type="email" placeholder="Email" required />
-              <input
-                type="password"
-                placeholder="Please enter your password"
-                required
-              />
-              <button className="--btn --btn-primary --btn-block">Login</button>
-              <div className="links">
-                <Link to="/Reset" style={{ color: "red" }}>
-                  Reset Password
-                </Link>
-              </div>
-              <p> --or--</p>
-              <button className="--btn --btn-danger --btn-block">
-                <IoLogoGoogle size={20} /> Login with Google
-              </button>
-              <span className="register">
-                Don't have an account?
-                <Link to="/Register" style={{ color: "red" }}>
-                  Register
-                </Link>
-              </span>
-            </form>
+    <>
+      <ToastContainer />
+      {isloading && <Loader />}
+      <section>
+        <div className="auth container">
+          <div className="img">
+            <img src={img_Login} alt="login" />
           </div>
-        </Card>
-      </div>
-    </section>
+          <Card>
+            <div className="form">
+              <h2>Login</h2>
+              <form onSubmit={fun_Login}>
+                <input type="email" placeholder="Email" required ref={Email} />
+                <input
+                  type="password"
+                  placeholder="Please enter your password"
+                  required
+                  ref={Password}
+                />
+                <button
+                  type="submit"
+                  className="--btn --btn-primary --btn-block"
+                >
+                  Login
+                </button>
+                <div className="links">
+                  <Link to="/Reset" style={{ color: "red" }}>
+                    Reset Password
+                  </Link>
+                </div>
+                <p> --or--</p>
+                <button className="--btn --btn-danger --btn-block">
+                  <IoLogoGoogle size={20} /> Login with Google
+                </button>
+                <span className="register">
+                  Don't have an account?
+                  <Link to="/Register" style={{ color: "red" }}>
+                    Register
+                  </Link>
+                </span>
+              </form>
+            </div>
+          </Card>
+        </div>
+      </section>
+    </>
   );
 };
 
--- newcourse/e_shop/src/pages/auth/Register.js
+++ newcourse/e_shop/src/pages/auth/Register.js
@@ -1,24 +1,65 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./auth_module.scss";
+import { auth } from "../firebase/config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import Loader from "../../Component/loader/Loader";
 import img_register from "../../assets/register.png";
 import Card from "../../Component/card/Card";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const Register = () => {
+  const Email = useRef(null);
+  const Password = useRef(null);
+  const ConfirmPassword = useRef(null);
+  const [isloading, setisLoading] = useState(false);
+  const navigate = useNavigate();
+  const RegisterUser = (e) => {
+    e.preventDefault();
+    const loginEmail = Email.current.value;
+    const currentPassword = Password.current.value;
+    const currentConfirmPassword = ConfirmPassword.current.value;
+    if (currentPassword !== currentConfirmPassword) {
+      toast.error("Password & Confirm Password do not match!!");
+    }
+    setisLoading(true);
+    createUserWithEmailAndPassword(auth, loginEmail, currentPassword)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setisLoading(false);
+        toast.success("Registration is successful!!");
+        navigate("/login");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+        setisLoading(false);
+      });
+  };
   return (
-    <section>
-      <div className="auth container">
+    <>
+      <ToastContainer />
+      {isloading && <Loader />}
+
+      <section className="auth container">
         <Card>
           <div className="form">
             <h2>Register</h2>
-            <form>
-              <input type="email" placeholder="Email" required />
+            <form onSubmit={RegisterUser}>
+              <input type="email" placeholder="Email" required ref={Email} />
               <input
                 type="password"
                 placeholder="Please enter your password"
                 required
+                ref={Password}
               />
-              <input type="password" placeholder="Confirm Password" required />
-              <button className="--btn --btn-primary --btn-block">
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                required
+                ref={ConfirmPassword}
+              />
+              <button type="submit" className="--btn --btn-primary --btn-block">
                 Register
               </button>
 
@@ -34,8 +75,8 @@ const Register = () => {
         <div className="img">
           <img src={img_register} alt="login" />
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
