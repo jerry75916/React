6158c4a5cf2bd83c0fdcb8cc4629b3a9485c51ff
HsiehJerry <jerry.hsieh@garmin.com>
2022-10-07 17:00:28 +0800

    sass

--- newcourse/e_shop/src/Component/Product/Product.jsx
+++ newcourse/e_shop/src/Component/Product/Product.jsx
@@ -1,38 +1,37 @@
-import React,{useEffect} from 'react'
-import styles from "./Product.module.scss"
-import ProductFilter from './ProductFilter/ProductFilter'
-import ProductList from './ProductList/ProductList'
-import { useSelector, useDispatch} from 'react-redux'
-import useFetchCollection from '../../customHooks/useFetchCollection'
-import { selectProducts, STORE_PRODUCTS } from '../../redux/slice/productSlice'
+import React, { useEffect } from "react";
+import styles from "./Product.module.scss";
+import ProductFilter from "./ProductFilter/ProductFilter";
+import ProductList from "./ProductList/ProductList";
+import { useSelector, useDispatch } from "react-redux";
+import useFetchCollection from "../../customHooks/useFetchCollection";
+import { selectProducts, STORE_PRODUCTS } from "../../redux/slice/productSlice";
 // import spinner from "../../assets/spinner.jpg"
-import Loader from '../loader/Loader'
+import Loader from "../loader/Loader";
 const Product = () => {
-
-  const {data,isLoading}= useFetchCollection("products");
-  const Products=useSelector(selectProducts)
-  const dispatch=useDispatch();
+  const { data, isLoading } = useFetchCollection("products");
+  const Products = useSelector(selectProducts);
+  const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(STORE_PRODUCTS({
-       products:data
-     }))
-   }, [dispatch,data]);
-
+    dispatch(
+      STORE_PRODUCTS({
+        products: data,
+      })
+    );
+  }, [dispatch, data]);
 
   return (
-    <section >
-        <h2>Product</h2>
-        <div className={`container ${styles.product}`}>
-            <aside className={styles.filter}>
-            {isLoading?null: (<ProductFilter />)}
-            </aside>
-            <div className={styles.content}>
-             {isLoading?<Loader/>:( <ProductList products={Products}/>)}
-            </div>
+    <section>
+      <h2>Product</h2>
+      <div className={`container ${styles.product}`}>
+        <aside className={styles.filter}>
+          {isLoading ? null : <ProductFilter />}
+        </aside>
+        <div className={styles.content}>
+          {isLoading ? <Loader /> : <ProductList products={Products} />}
         </div>
-        
-        </section>
-  )
-}
+      </div>
+    </section>
+  );
+};
 
-export default Product
+export default Product;
